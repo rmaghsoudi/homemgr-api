aa8f9da6d02edb901e1daee3ca62f25b366d1968
rmaghsoudi <romy_mag@outlook.com>
2019-07-29 11:31:35 -0400

    added require to user params

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -22,6 +22,6 @@ class Api::UsersController < ApplicationController
  
   private
   def user_params
-    params.permit(:username, :password, :first_name, :last_name, :avatar)
+    params.require(:user).permit(:username, :password, :first_name, :last_name, :avatar)
   end
 end
