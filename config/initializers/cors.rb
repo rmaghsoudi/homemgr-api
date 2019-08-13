d44a2443f536db5961e6d29d5cbc766f17e3c9a5
rmaghsoudi <romy_mag@outlook.com>
2019-08-13 18:23:11 -0400

    changed cors to deployed app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://homemgr-4a8ec.firebaseapp.com'
 
     resource '*',
       headers: :any,
